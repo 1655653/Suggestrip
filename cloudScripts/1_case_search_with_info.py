5bcf8522663f7d86ee9e1b330fe406b3174e6af4
LuigiSigillo <luigisigillo30@gmail.com>
2020-11-09 12:50:13 +0100

    changed lambda

--- cloudScripts/1_case_search_with_info.py
+++ cloudScripts/1_case_search_with_info.py
@@ -2,9 +2,18 @@ import json
 from botocore.vendored import requests
 import os
 from math import sin, cos, sqrt, atan2, radians
+import math
 import boto3
+from decimal import Decimal
+from pprint import pprint
 
-
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return float(obj)
+        return super(CustomJsonEncoder, self).default(obj)
+        
+        
 def get_distance(lat1,lon1,lat2,lon2):
     # approximate radius of earth in km
     R = 6373.0
@@ -24,106 +33,112 @@ def get_distance(lat1,lon1,lat2,lon2):
 
 
 def get_weather(lat, lon):
-    return requests.get(f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid='+os.environ['open_weather']).json()
+    r =  requests.get(f'http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid='+os.environ['open_weather']).json()
+    payload = {}
+    payload['weather'] = r['weather']
+    payload['temperature'] = int(float(r['main']['temp']) - 273.15)
+    return payload
+    
 
+def evaluate_tags(request, city):
+    tags_req = request['tags']
+    tags_city = city['tags']
+    tags_score = 0
+    for tag_name, tag_value in tags_req.items():
+        if tags_city[tag_name] >= tag_value:
+            tags_score += 5 + tag_value * 5
+        else:
+            distance = tag_value - tags_city[tag_name]
+            tags_score += 5 + tag_value * 5 - distance * 5
+    #print("tag score ", tags_score)
+    return tags_score
+    
+    
+def evaluate_costs(request, city):
+    cost_req = request['cost']
+    cost_city = city['tags']['costs']
+    dist = abs(cost_req - cost_city)
+    similarity = (2 - dist) * 25
+    #print("cost score", similarity)
+    return similarity
+    
+    
+def evaluate_distance(request, city):
+    #y different names
+    dist = get_distance(request['gps_coords'][0]['lat'],request['gps_coords'][0]['lon'],city['coordinates'][0]['lat'],city['coordinates'][0]['lon'])
+    cost = request['cost']
+    if cost == 2:
+        score = 20 - int(math.log(dist))
+    elif cost == 1:
+        score = 20 - 2*int(math.log(dist))
+    else:
+        score = 20
+    return score
+    
+    
+def evaluate_weather(request, city):
+    weather = get_weather(city['coordinates'][0]['lat'],city['coordinates'][0]['lon']) 
+    #pprint(weather)
+    score = weather['weather'][0]['id'] * 5/100 + (int(weather['temperature'] / 5) + 1 if weather['temperature']< 24 else 9 - int(weather['temperature'] / 5))*2
+    #score = 800 *5/100 + (int(30 / 5) + 1 if 30 < 24 else 9 - int(30 / 5))*2 
+    return score
+    
+    
+def get_user_city_score(request, city):
+    if request['flag_lastminute'] == "True":
+        score = evaluate_tags(request,city) + evaluate_costs(request,city) + evaluate_distance(request, city) + evaluate_weather(request, city) 
+        norm_score = float(score)/3
+    else:
+        score = evaluate_tags(request,city) + evaluate_costs(request,city) + evaluate_distance(request, city)
+        norm_score = float(score)/2.5
+    return norm_score
+    
 
+def rank(request, db):
+    response_body = {}
+    lst = []
+    OldMax = 0
+    OldMin = 100
+    for city in db:
+        record = {}
+        record['name'] = city['name']
+        record['id'] = city['ID']
+        record['tags'] = city['tags']
+        record['user_tags'] = request['tags']
+        record['tag_score'] = evaluate_tags(request,city)
+        record['weather'] = get_weather(city['coordinates'][0]['lat'],city['coordinates'][0]['lon']) 
+        record['weather_score'] = evaluate_weather(request, city)
+        record['distance'] = get_distance(request['gps_coords'][0]['lat'],request['gps_coords'][0]['lon'],city['coordinates'][0]['lat'],city['coordinates'][0]['lon'])
+        record['distance_score'] = evaluate_distance(request, city)
+        record['cost'] = request['cost']
+        record['cost_score'] = evaluate_costs(request, city)
+        record['score'] = get_user_city_score(request, city)
+        OldMin = OldMin if OldMin < record['score'] else record['score']
+        OldMax = OldMax if OldMax > record['score'] else record['score'] 
+        lst.append(record)
+    OldRange = (OldMax - OldMin)  
+    NewRange = 100
+    for elem in lst:
+        print(elem['score'])
+        elem['score'] = (((elem['score'] - OldMin) * NewRange) / OldRange)
+        print(elem['score'])
+    from operator import itemgetter
+    ranked_list = sorted(lst, key=itemgetter('score'), reverse=True) 
+    #pprint(ranked_list[:10])
+    return ranked_list[:10]
+    
+    
 def lambda_handler(event, context):
     
-    
-    request_body = json.loads(event['body'])
-    #FOR TESTING IN LAMBDA CONSOLE USE THIS
-    '''request_body = {
-"msg_id":"01, codice del messaggio, esempio 01 per messaggio caso uso 1",
-"gps_coords":[
-    {
-        "lat":48.856613,
-        "lon":2.352222,
-        "primary":"",
-        "globe":"earth"
-    }
-],
-"flag_lastminute":"True",
-"tags":[
-    {
-        "night_life":"3, an int from 1 to 5 ",
-        "nature":"3, an in from 1 to 5",
-        "sports":"3, an int from 1 to 5",
-        "food":"3, an int from 1 to 5",
-        "culture":"3, an int from 1 to 5",
-        "infrastructure":"3, an int from 1 to 5",
-        "costs":"2, an int from 1 to 3"
-    }
-],
-"target":{
-    "travel_type":"identifier of family, couple, friends",
-    "age_type":"ages"
-}
-}'''
-    user_position = request_body['gps_coords'][0]
-    is_last_minute = request_body['flag_lastminute']
-    
-    response_body = {}
-    response_body['ranking_list'] = []
-    
-    
-    # TODO implement algo
-    ranking_object = {
-        "brief_description": "The Eternal City has always been coveted. These days, the bounty is an immersive step back in time.",
-        "coordinates": [
-            {
-                "globe": "earth",
-                "lat": 41.88333333,
-                "lon": 12.5,
-                "primary": ""
-            }
-        ],
-        "description": "Few cities serve up the ability to walk the history of the Western world like Roma. Heck, Palatine Hill alone invites you into two millennia of Western civilization, if you’ve got an hour. Mix a safe, accessible modern city and its thousands of portals back in time and it’s easy to see how Rome almost cracked the Best Cities Top 10 again this year. Declarations of love for the city have multiplied with the channels of self-expression, of course, and the city’s #7 ranking in our extensive Place category has directly fueled its #5 Promotion ranking, including the second-most number of TripAdvisor reviews on the planet and very frequent Google searches. The curiosity about the Eternal City will only increase as Rome reopens carefully to visitors, who, after gorging on six months of local ’Grams featuring empty summer streets and iconic sites with nary a tourist umbrella to clutter the shot, are keen to time their return before the crowds come back. As of late summer 2020, most sights were reopened, as were most bars and restaurants. It’s not exactly la dolce vita quite yet, but Roma has persevered once again.",
-        "name": "Rome",
-        "tags": {
-            "costs": 3,
-            "culture": 4,
-            "food": 3,
-            "infrastructure": 4,
-            "nature": 1,
-            "night_life": 1,
-            "sports": 3
-        },
-        "country": "IT"
-    }
-    
-    #GET from DB
-    # Get the service resource.
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('test_suggestrip')
+    response = table.scan()
+    response = json.loads(json.dumps(response['Items'], cls=DecimalEncoder))
+    #pprint(response)
+    request_body = json.loads(event['body'])
+    
 
-    response = table.get_item(Key={'ID': '23'})
-    item = response['Item']
-    print(item)
-    
-    #RETRIEVE COVID INFO
-    covid_info = requests.get('http://corona-api.com/countries/'+ranking_object['country']).json()
-    our_covid_info_city = {}
-    our_covid_info_city['city'] = ranking_object['name']
-    our_covid_info_city['date'] = covid_info['data']['updated_at']
-    our_covid_info_city['today'] = covid_info['data']['today']
-    our_covid_info_city['latest_data'] = covid_info['data']['latest_data']
-    
-    ranking_object['covid_info'] = our_covid_info_city
-    
-    #RETRIEVE Wheather info
-    if is_last_minute =="True":
-        weather = get_weather(ranking_object["coordinates"][0]['lat'],ranking_object["coordinates"][0]['lon'])
-        ranking_object['weather'] = weather
-    
-    
-    
-    # Retrieve distance in Kmeters
-    ranking_object['distance'] = get_distance(user_position['lat'],user_position['lon'], ranking_object['coordinates'][0]['lat'],ranking_object['coordinates'][0]['lon'])
-    
-    
-    
-    response_body['ranking_list'].append(ranking_object)
-    
+    response_body = rank(request_body, response)
     return {
         'statusCode': 200,
         'body': json.dumps(response_body),
